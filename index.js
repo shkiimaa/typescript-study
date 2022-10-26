0fbf6019750b7eab81c0415702f6a743c4f576d4
SuHwan,Kim (김수환) <shkiimaa@gmail.com>
2022-10-26 15:52:17 +0900

    타입스트립트 기본 문법

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-var userName = ['kim', 'park'];
+var userName = 'kim';
+var test = 'kim';
+//array
+var userName2 = ['kim', 'park'];
+//type을 2가지 이상 사용할때
+var userName3 = ['kim', 123];
+//object
 var age = { age: 29 };
+//속성을 옵션으로 사용할때
+var age2 = { age: 29 };
+//function
+function plus(x) {
+    return x + 2;
+}
+var project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true,
+};
+// project 변수 우측에 적으면 됩니다.
+// member 안엔 문자로 가득한 array만 들어올 수 있고
+// days는 숫자, started는 true/false만 들어올 수 있습니다.
--- index.ts
+++ index.ts
@@ -1,8 +1,30 @@
-let userName: string[] = ['kim', 'park'];
-let age: { age: number } = { age: 29 };
+let userName: string = 'kim';
 
+let test = 'kim';
+
+//array
+let userName2: string[] = ['kim', 'park'];
+//type을 2가지 이상 사용할때
+let userName3: (string | number)[] = ['kim', 123];
+
+//object
+let age: { age: number } = { age: 29 };
+//속성을 옵션으로 사용할때
+let age2: { age?: number } = { age: 29 };
+
+//function
 function plus(x: number): number {
   return x + 2;
 }
 
-plus('name');
+let project: { member: string[]; days: number; started: boolean } = {
+  member: ['kim', 'park'],
+  days: 30,
+  started: true,
+};
+
+// project 변수 우측에 적으면 됩니다.
+
+// member 안엔 문자로 가득한 array만 들어올 수 있고
+
+// days는 숫자, started는 true/false만 들어올 수 있습니다.
